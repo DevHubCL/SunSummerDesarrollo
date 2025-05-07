015eeb8b6a0f9d3a6140bf327c1dbc31495f28db
PsychoKillerdd <onlyvicenteuribe@gmail.com>
2025-05-06 21:52:57 -0400

    Banner Y navigationbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Hero from './components/Hero'
+import NavigationBar from './components/NavigationBar'
 import './index.css'
 function App() {
 
   return (
     <>
+    <div className='fixed z-50 w-full'>
+    <NavigationBar/>
+    </div>
     <h1 className="text-2xl bg-black text-center text-amber-300 ">hola</h1>
+     <div className='z-10'>
      <Hero/>
+     </div>
     </>
   )
 }
